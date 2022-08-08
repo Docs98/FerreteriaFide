d65dbe55e2f96ef1b10dec2be6e0665a7db46d5c
kevin fallas alfaro <kfallas40817@ufide.ac.cr>
2022-08-07 23:31:49 -0600

    agregando cambios del migration

--- FerreteriaFide.Infraestructura/Data/ApplicationDbContext.cs
+++ FerreteriaFide.Infraestructura/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using FerreteriaFide.Aplicacion.Data;
+using FerreteriaFide.Domain.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,11 @@ namespace FerreteriaFide.Infraestructura.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
              : base(options) { }
+
+        public DbSet<Producto> productos { get; set; }
+        public DbSet<Usuarios> usuarios { get; set; }
+        public DbSet<Roles> roles { get; set; }
+        public DbSet<Marca> marca { get; set; }
+        public DbSet<Proveedor> proveedor { get; set; }
     }
 }

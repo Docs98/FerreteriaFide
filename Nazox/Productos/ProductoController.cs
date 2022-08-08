ed4069538f440f3fc83671c2001e5c74ab8e07be
kevin fallas alfaro <kfallas40817@ufide.ac.cr>
2022-08-07 21:58:59 -0600

    Agregando frontend

---
+++ Nazox/Productos/ProductoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ferreteria_Fide.Productos
+{
+    public class ProductosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

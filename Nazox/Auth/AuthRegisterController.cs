51f44c49b673c9e891a252e864c1427f7c302681
kevin fallas alfaro <kfallas40817@ufide.ac.cr>
2022-08-24 23:45:39 -0600

    agregando el register

--- Nazox/Auth/AuthRegisterController.cs
+++ Nazox/Auth/AuthRegisterController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FerreteriaFide.Domain.Models;
+using FerreteriaFide.Infraestructura.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +11,28 @@ namespace Nazox.Auth
 {
     public class AuthRegisterController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public AuthRegisterController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Register(Usuarios usuario)
+        {
+            if (usuario.roles == null)
+            {
+                usuario.roles = new FerreteriaFide.Infraestructura.Clientes.RolesClientes(_context).GetRoles(3);
+                new FerreteriaFide.Infraestructura.Clientes.UsuariosCliente(_context).AddUsuario(usuario);
+                return RedirectToAction("Index", "AuthLogin");
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
